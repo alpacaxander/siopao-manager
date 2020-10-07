2471494defc1db2815ccaf2c44a3ab97bd5331c5
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2020-10-07 13:10:58 -0700

    inventory component and service init

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {InventoryService} from '../services/inventory.service';
-import {Coin} from '../resources/coin';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.scss']
 })
+
 export class InventoryComponent implements OnInit {
 
-
-  constructor( private coinService: InventoryService) { }
+  constructor( private inventoryService: InventoryService) { }
 
   ngOnInit(): void {}
 
