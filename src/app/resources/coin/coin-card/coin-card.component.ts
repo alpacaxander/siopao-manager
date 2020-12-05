adb7be35a3b9ab0a9d7801ff9d8878e266f9f970
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2020-12-04 17:12:41 -0800

    use coin cards instead of table

--- src/app/resources/coin/coin-card/coin-card.component.ts
+++ src/app/resources/coin/coin-card/coin-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core'
 import { Coin } from '../coin'
 import { InventoryService } from '../../../services/inventory.service'
 import { Image } from '../../image/image'
@@ -8,13 +8,19 @@ import { Image } from '../../image/image'
              templateUrl: './coin-card.component.html',
              styleUrls: ['./coin-card.component.scss'],
            })
-export class CoinCardComponent implements OnInit {
+export class CoinCardComponent implements OnInit, OnChanges {
 
-  @Input()
-  coin: Coin
+  coinValue: Coin
 
-  @Output()
-  onChange: EventEmitter<void> = new EventEmitter<void>()
+  @Output() coinChange: EventEmitter<Coin> = new EventEmitter<Coin>()
+
+  @Input() get coin(): Coin {
+    return this.coinValue
+  }
+
+  set coin(val) {
+    this.coinValue = val
+  }
 
   selected: number = 0
 
@@ -22,15 +28,16 @@ export class CoinCardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.inventoryService.read<Image[]>(this.coin.relationships.images.links.related).then(
-      (images: Image[]) => {
-        this.coin.relationships.images.data = images
-      }
-    )
   }
 
-  public update() {
-    this.onChange.emit()
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.coin.relationships) {
+      this.inventoryService.read<Image[]>(this.coin.relationships.images.links.related).then(
+        (images: Image[]) => {
+          this.coin.relationships.images.data = images
+        }
+      )
+    }
   }
 
 }
