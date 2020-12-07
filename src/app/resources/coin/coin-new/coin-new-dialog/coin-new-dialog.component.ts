9602c45d566c75f1b997cf3543bdc21d84ae7be1
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2020-12-07 15:07:31 -0800

    mat dialog on create new coin

---
+++ src/app/resources/coin/coin-new/coin-new-dialog/coin-new-dialog.component.ts
@@ -0,0 +1,70 @@
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core'
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog'
+import { Coin } from '../../coin'
+import { Image } from '../../../image/image'
+import { Product } from '../../../product/product'
+import { FormControl } from '@angular/forms'
+import { InventoryService } from '../../../../services/inventory.service'
+
+@Component({
+  selector: 'app-coin-new-dialog',
+  templateUrl: './coin-new-dialog.component.html',
+  styleUrls: ['./coin-new-dialog.component.scss']
+})
+export class CoinNewDialogComponent implements OnInit {
+
+  public conditionFormControl = new FormControl()
+  public conditionEnum: string[] = ['Good', 'Great'];
+
+  public statusFormControl = new FormControl()
+  public statusEnum: string[] = ['Ordered from vendor', 'In Stock (unprocessed)', 'In Stock', 'Sold', 'Dropped off for delivery', 'Delivered'];
+
+  public location
+
+  public bundles: Image[][] = []
+
+  public onCreate: EventEmitter<void> = new EventEmitter<void>()
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public product: Product,
+    private inventoryService: InventoryService,
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+  public send(): void {
+    for (let bundle of this.bundles) {
+      this.inventoryService.create(
+        {
+          type: 'coin',
+          // @ts-ignore
+          attributes: {
+            status: this.statusFormControl.value ? this.statusFormControl.value : '',
+            location: this.location,
+            condition: this.conditionFormControl.value ? this.conditionFormControl.value : '',
+          },
+          relationships: {
+            // @ts-ignore
+            product: {
+              data: this.product
+            }
+          }
+        }
+      ).then(
+        (coin: Coin) => {
+          for (const image of bundle) {
+            image.relationships = {
+              // @ts-ignore
+              coin: {
+                data: coin
+              }
+            }
+            this.inventoryService.create(image).then()
+          }
+          this.onCreate.emit()
+        }
+      )
+    }
+  }
+}
--- src/app/resources/coin/coin-new/coin-new.component.ts
+++ src/app/resources/coin/coin-new/coin-new.component.ts
@@ -1,9 +1,7 @@
-import { Coin } from './../coin'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap'
-import { InventoryService } from '../../../services/inventory.service'
 import { Product } from '../../product/product'
-import { Image } from '../../image/image'
+import { MatDialog } from '@angular/material/dialog'
+import { CoinNewDialogComponent } from './coin-new-dialog/coin-new-dialog.component'
 
 @Component({
              selector: 'app-coin-new',
@@ -12,88 +10,24 @@ import { Image } from '../../image/image'
            })
 export class CoinNewComponent implements OnInit {
 
-  public closeResult = ''
-
-  public bundles: Image[][] = []
-
   @Input()
   public product: Product
 
   @Output()
   public onCreate: EventEmitter<void> = new EventEmitter<void>()
 
-  public coin: Coin
-
   constructor(
-    private inventoryService: InventoryService,
-    private modalService: NgbModal,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
-    this.reset()
   }
 
-  open(content): void {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`
-    })
+  openDialog() {
+    const dialog = this.dialog.open(
+      CoinNewDialogComponent,
+      {data: this.product},
+    )
+    dialog.componentInstance.onCreate.subscribe(() => {this.onCreate.emit()})
   }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC'
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop'
-    } else {
-      return `with: ${reason}`
-    }
-  }
-
-  private send(): void {
-    for (let bundle of this.bundles) {
-      this.inventoryService.create(this.coin).then(
-        (coin: Coin) => {
-          for (const image of bundle) {
-            image.relationships = {
-              // @ts-ignore
-              coin: {
-                data: coin
-              }
-            }
-            this.inventoryService.create(image).then()
-          }
-          this.reset()
-          this.onCreate.emit()
-        }
-      )
-    }
-  }
-
-  private reset(): void {
-    // @ts-ignore
-    this.coin = {
-      type: 'coin',
-      id: '',
-      attributes: {
-        status: '',
-        location: '',
-        condition: '',
-        craigslistLink: '',
-        facebookLink: '',
-      },
-      relationships: {
-        // @ts-ignore
-        images: {
-          data: []
-        },
-        // @ts-ignore
-        product: {
-          data: this.product
-        }
-      }
-    }
-  }
-
 }
