f35b3c14ff948db26c2a1438fbd51b49935942c1
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2020-12-11 19:38:03 -0800

    json format issue

--- config.sh
+++ config.sh
@@ -2,6 +2,6 @@
 
 mkdir -p $STATIC_LOCATION/config/
 
-echo { >> $STATIC_LOCATION/config/properties.json
-echo \"INVENTORY_URL\": $INVENTORY_URL >> $STATIC_LOCATION/config/properties.json
-echo } >> $STATIC_LOCATION/config/properties.json
+echo "{" >> $STATIC_LOCATION/config/properties.json
+echo \"INVENTORY_URL\": \"$INVENTORY_URL\" >> $STATIC_LOCATION/config/properties.json
+echo "}" >> $STATIC_LOCATION/config/properties.json
